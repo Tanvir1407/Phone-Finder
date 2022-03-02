559d3c395af7076614709b304dfa681399a292a0
MD. Tanvir <mt14050@gmail.com>
2022-03-02 12:32:25 +0600

    complete api intrigation to get phone

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,37 @@
 
 const findPhone = (phoneName) => {
-    fetch('https://openapi.programming-hero.com/api/phones?search=${phoneName}.data')
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${phoneName}`)
         .then(res => res.json())
-        .then(phone => console.log(phone))
+        .then(phone => displayPhone(phone.data))
 };
 
 const searchPhone = () => {
     const phoneNameText = document.getElementById('input-filed');
     findPhone(phoneNameText.value);
+    phoneNameText.value = '';
+}
+
+const displayPhone = (phoneInfo) => {
+    const showPhone = document.getElementById('show-phone');
+    showPhone.textContent = '';
+    phoneInfo.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col-lg-4');
+        div.classList.add('col-12');
+        div.classList.add('col-md-6');
+        div.innerHTML = `
+            <div class="card">
+                <div class="text-center">
+                    <img src="${phone.image}">
+                </div>
+                <h3 class="text-center phone-name">${phone.phone_name}</h3>
+                <div class="text-box">
+                    <p class="brand-name">${phone.brand}</p>
+                    <button class="details-btn">Details</button>
+                </div>
+            </div>
+        `
+        showPhone.appendChild(div);
+    })
 }
